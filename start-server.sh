e7ddb7482e7fa9618fb4ece0cc5e787159212d94
Kiltzx <dudu.cedsv@gmail.com>
2022-11-15 19:16:54 -0300

    added versions

--- start-server.sh
+++ start-server.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+version_mine="server-1-19-2"
+
+echo "What version wanna run minecraft-server ?"
+echo "1 - 1.19.2 (Default)"
+echo "2 - 1.12.2"
+read version
 
 echo "Who much ram allocate to server ? (4096M)"
 read ramsize
 
+if [ ramsize == "" ] || [ ramsize == "0" ]
+then
+	ramsize="4096M"
+fi
+
+if [ $version == "2" ] || [ $version == "1.12.2" ]
+then
+	version_mine="server-1-12-2"	
+fi
+
 cd server-data
-java -Xmx$ramsize -Xms$ramsize -jar server-1-19-2.jar nogui
+java -Xmx$ramsize -Xms$ramsize -jar server-versions/$version_mine.jar nogui --world world_$version_mine
 
 echo "Wanna backup world ?(N/y)"
 read backup
@@ -14,10 +30,9 @@ then
 	echo "Backup files!!"
 	echo "Running..."
 	dir_name=$(date +%d_%m_at_%H:%M)
-	echo $dir_name
 	mkdir $dir_name
-	cp -r world $dir_name/
-	rar a ../backups/$dir_name.zip $dir_name
+	cp -r world_$version_mine $dir_name/
+	rar a ../backups/world_$dir_name-$version_mine.rar $dir_name
 	rm -rf $dir_name
 	echo "Backup success!!"
 fi
